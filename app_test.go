241a2c729c6a23d083a19c395bc066a9d9dd2f5b
Yann Rolland <yann.rolland@iadvize.com>
2022-06-06 22:29:38 +0200

    wip

--- app_test.go
+++ app_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestAllPossibleSplits(t *testing.T) {
 
-	initialDistrict := makeDistrict(6, 6)
+	initialDistrict := makeDistrict(7, 6)
 	initialDistricts := []District{initialDistrict}
 	allPossibleSplits := findAllSplits(initialDistricts, 0)
 
 	// print all possible splits
 	for _, split := range allPossibleSplits {
-		debug("split:", split)
+		// debug("split:", split)
 
 		assert(districtsSize(split) == districtSize(initialDistrict))
 	}
